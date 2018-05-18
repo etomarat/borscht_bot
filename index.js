765a346aa349c23d809aff74624bf8716ab0ac27
etomarat <etomarat@gmail.com>
2018-05-18 13:29:03 +0300

    its worked

---
+++ index.js
@@ -0,0 +1,61 @@
+const TeleBot = require('telebot');
+const GoogleImages = require('google-images');
+const _ = require('lodash');
+const uuidv4 = require('uuid/v4');
+const searchClient = new GoogleImages('014068829970830705133:0sbqksb7ldw', 'AIzaSyANLTy1iZCDapwkNpTkmq8_6n90XFCZdKw');
+
+const bot = new TeleBot('584357222:AAEUw8rFwnF0mJbB1Kw8tOxeTaVGNcb1MTY');
+
+const soupDict = [
+    'Борщ',
+    'Грибной',
+    'Крем супный',
+    'Зеленый борщ',
+    'Солянка',
+    'Фасолевый',
+    'Гороховый',
+    'Харчо',
+    'C фрикадельками'
+]
+
+// On inline query
+bot.on('inlineQuery', msg => {
+
+    let query = msg.query;
+    console.log(`inline query: ${ query }`);
+
+    // Create a new answer list object
+    const answers = bot.answerList(msg.id, {cacheTime: 1});
+    const youChose = query && `Я вижу ты хочешь ${query}`;
+    const botChose = _.sample(soupDict);
+    // Article
+    answers.addArticle({
+        id: 'query',
+        title: youChose || `Я выбрал тебе суп: ${botChose}`,
+        description: `Если хочешь другой, то напиши название`,
+        message_text: 'Жми!!!'
+    });
+
+    const searchQuery = query || botChose
+    console.log(searchQuery, query);
+    
+    return searchClient.search(searchQuery)
+        .then(images => {
+            _.shuffle(images).forEach((image, i) => {
+                answers.addPhoto({
+                    id: uuidv4().split('-')[0],
+                    caption: image.description,
+                    photo_url: image.url,
+                    // thumb_url: image.url,
+                    thumb_url: image.thumbnail.url,
+                    photo_height: 100,
+                    photo_width: 200
+                });
+            });
+            
+            return bot.answerQuery(answers);
+        });
+
+});
+
+bot.start();
